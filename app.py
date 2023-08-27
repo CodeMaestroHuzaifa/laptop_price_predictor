3c7c3a5e4864ff28025a8948ebb9b3e302e79785
CodeMaestroHuzaifa <143184508+CodeMaestroHuzaifa@users.noreply.github.com>
2023-08-27 19:52:46 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 from model import df,pipe
 import numpy as np
 import streamlit as st
-# hide_button = """
-# <style>
-# button{
-#   visibility:hidden;
-# }
+hide_button = """
+<style>
+button{
+  visibility:hidden;
+}
 
-# </style>
+</style>
 
-# """ 
+""" 
 
 st.title("Laptop Price Predictor")
 
-# st.markdown(hide_button, unsafe_allow_html=True)
+st.markdown(hide_button, unsafe_allow_html=True)
 
 #Manufacturer
 brand = st.selectbox('Manufacturer',df['Company'].unique())
